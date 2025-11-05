6dec22b96bfdb96888ff8afcfac3bbc118b215fa
Ivan Almer <ivan.almer@gmail.com>
2025-11-05 21:28:53 +0100

    get back bashls

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -3,7 +3,7 @@ require("mason").setup()
 
 -- mason-lspconfig setup
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "pyright", "stylua" }, -- auto-install these
+  ensure_installed = { "lua_ls", "pyright", "stylua", "bashls" }, -- auto-install these
   automatic_installation = true, -- automatically install configured servers
 })
 
